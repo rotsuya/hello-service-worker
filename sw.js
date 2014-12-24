5463b9093a107b02c416f3b00b136d54f4bde0a5
Ryosuke Otsuya <r.otsuya@gmail.com>
2014-12-24 12:11:43 +0900

    キャッシュを登録

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
-/* */
+var CACHE_NAME = 'my-site-cache-v1';
+
+// The files we want to cache
+var urlsToCache = [
+    'index.html',
+    'script.js'
+];
+
+// Set the callback for the install step
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
