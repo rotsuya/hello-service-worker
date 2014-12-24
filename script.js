a5a2c7148f282ab0c2cad87f7c54e46e5ed2985b
Ryosuke Otsuya <r.otsuya@gmail.com>
2014-12-24 12:10:58 +0900

    sw.jsを絶対パスで指定

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 if ('serviceWorker' in navigator) {
     console.log('ServiceWorker is available.');
-    navigator.serviceWorker.register('sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-        // Registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-    });
+    navigator.serviceWorker
+        .register('/hello-service-worker/sw.js')    // 絶対パス
+        .then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(function(err) {
+            // Registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
 } else {
     console.log('ServiceWorker is NOT available.');
 }
